6fc37c5b9d28c07bdd13eb1d8d54f4dfbf9dc394
TebaHuido <baase.hui@gmail.com>
2022-06-24 10:37:35 -0400

    sistema de subida a mysql

--- bd.php
+++ bd.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=quokka', 'root', '');
+    $pdo = new PDO('mysql:host=localhost;dbname=lab04', 'root', '');
 } catch (PDOException $e) {
     print "¡Error!: " . $e->getMessage() . "<br/>";
     die();
--- index.php
+++ index.php
@@ -7,26 +7,27 @@
     <title>Document</title>
 </head>
 <body>
-    <form>
-        <input class="form-control" type="text" placeholder="Nombre"><br>
-        <input class="form-control" type="number" placeholder="Precio"><br>
-        <input type="file" name="image"/><br>
+    <form action="./subir.php" method="POST" enctype="multipart/form-data">
+        <input required name="nombre" class="form-control" type="text" placeholder="Nombre"><br>
+        <input required name="precio" class="form-control" type="number" placeholder="Precio"><br>
+        <input required type="file" name="imagen"/><br>
+        <textarea required name="descripcion"  rows="4" cols="50" placeholder="Descripcion del producto"></textarea><br>
             <!--Selector para categoria -->
-        <select class="form-control form-control-sm">
+        <select required name="categoria" class="form-control form-control-sm">
             <option>categoria 1</option>
             <option>categoria 2</option>
             <option>categoria 3</option>
         </select><br>
-        <input class="form-control" type="date" placeholder="Fecha de ingreso"><br>
+        <input required name="fecha" class="form-control" type="date" placeholder="Fecha de ingreso"><br>
 
-        <input type="radio" id="temp1" name="temporada" value="temporada1">
+        <input required type="radio" id="temp1" name="temporada" value="temporada1">
         <label for="temp1">temporada1</label><br>
-        <input type="radio" id="temp2" name="temporada" value="temporada2">
+        <input required type="radio" id="temp2" name="temporada" value="temporada2">
         <label for="temp2">temporada2</label><br>
-        <input type="radio" id="temp3" name="temporada" value="temporada3">
-        <label for="temp3">temporada3</label><br>
+        <input required type="radio" id="temp3" name="temporada" value="temporada3">
+        <label required for="temp3">temporada3</label><br>
         <input type="radio" id="temp4" name="temporada" value="temporada4">
-        <label for="temp4">temporada4</label><br><br>
+        <label required for="temp4">temporada4</label><br><br>
         <input type="submit" value="Submit">
     </form>
 </body>
